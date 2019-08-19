64e8e3346ea9e51af06d42250f8f4307df4f5d77
Yuhao <jimfengyh@gmail.com>
2019-08-19 15:56:32 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+function Todo({ todo, index, completedTodo, removeTodo }) {
+  return (
+    <div style={{color: todo.isCompleted ? 'green' : 'red'}}>
+      {
+        todo.content
+      }
+      &nbsp;&nbsp;
+      <button onClick={ () => completedTodo(index)}>Done</button>
+      <button onClick={ () => removeTodo(index)}>Delete</button>
+    </div>
+  )
+}
+
+function TodoForm({ addTodo }) {
+  const [ value, setValue ] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
+}
 
 function App() {
+  const [ todos, setTodos ] = useState([
+    {
+      content: 'Eat',
+      isCompleted: false,
+    },
+    {
+      content: 'Drink',
+      isCompleted: true,
+    },
+    {
+      content: 'Sleep',
+      isCompleted: false,
+    },
+    {
+      content: 'Walk',
+      isCompleted: false,
+    }
+  ]);
+
+  const addTodo = content => {
+    const newTodos = [ ...todos, { content } ];
+    setTodos(newTodos);
+  }
+
+  const completedTodo = index => {
+    const newTodos = [ ...todos ];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
+  const removeTodo = index => {
+    const newTodos = [ ...todos ];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {
+          todos.map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completedTodo={completedTodo}
+              removeTodo={removeTodo}
+            />
+          ))
+        }
+        <TodoForm addTodo={addTodo}/>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
